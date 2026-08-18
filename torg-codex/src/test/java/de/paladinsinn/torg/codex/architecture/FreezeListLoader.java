af1186ca5ac43c7ff01a3dd2a21274813156ce69
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2026-08-18 10:17:23 +0200

    chore: add copyright notice to all files [skip ci]

--- torg-codex/src/test/java/de/paladinsinn/torg/codex/architecture/FreezeListLoader.java
+++ torg-codex/src/test/java/de/paladinsinn/torg/codex/architecture/FreezeListLoader.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2026.  Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * You may contact me via email rlichti@kaiserpfalz-edv.de or via mail
+ *
+ * Kaiserpfalz EDV-Service
+ * Roland T. Lichti
+ * Darmstädter Str. 12
+ * 64625 Bensheim
+ * GERMANY
+ */
+
 package de.paladinsinn.torg.codex.architecture;
 
 import java.io.IOException;
